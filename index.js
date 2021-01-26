af012dad8f841a2d64aa1837288fb600b260a0ac
Din Fikri <fikridin22@gmail.com>
2021-01-26 17:21:35 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./JENNIE - SOLO MV.mp4');
+
+readStream.on('data', (chunk) => {
+    console.log('size: ', chunk.length)
+});
+
+readStream.on('end', () => {
+    console.log(('stream is finished'))
+});
+
+readStream.on('error', (err) => {
+    console.log("an error has occuered.")
+    console.log(err)
+});
+
+readStream.pause()
+process.stdin.on('data', (chunk) => {
+    if (chunk.toString().trim() === 'finished') {
+        readStream.resume()
+    }
+    readStream.read()
+})
